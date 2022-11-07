c009634d2bd8ace341888b09dba216d4ee9c860f
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-07 14:42:17 +0530

    change font to bold

--- Dom/app.js
+++ Dom/app.js
@@ -1,5 +1,12 @@
-let title=document.getElementById('header-title');
-title.style.borderBottom="2px solid black";
+// let title=document.getElementById('header-title');
+// title.style.borderBottom="2px solid black";
 
-let itemTitle=document.querySelector('.title');
-itemTitle.style.color='green';
+// let itemTitle=document.querySelector('.title');
+// itemTitle.style.color='green';
+
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(let i=0;i<items.length;i++){
+items[i].style.fontWeight='bold';
+}
