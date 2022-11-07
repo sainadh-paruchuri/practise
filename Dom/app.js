cd44e364dcc71d337773241f97b811f9fb86d59f
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-07 20:14:26 +0530

    new content added

--- Dom/app.js
+++ Dom/app.js
@@ -4,9 +4,20 @@
 // let itemTitle=document.querySelector('.title');
 // itemTitle.style.color='green';
 
-let items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
+// let items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
 
-for(let i=0;i<items.length;i++){
-items[i].style.fontWeight='bold';
+// for(let i=0;i<items.length;i++){
+// items[i].style.fontWeight='bold';
+// }
+
+let item=document.getElementsByClassName('list-group-item');
+
+for(let i=0;i<item.length;i++){
+    item[i].style.background='red';
+}
+
+let tagName=document.getElementsByTagName('li');
+for(let i=0;i<tagName.length;i++){
+    tagName[i].style.color='green';
 }
