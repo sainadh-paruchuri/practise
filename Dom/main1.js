906887ebb7572d05500b526361e1134310052b6e
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-11-10 17:26:38 +0530

    local storage show on webpage

--- Dom/main1.js
+++ Dom/main1.js
@@ -4,13 +4,23 @@ const emailInput=document.querySelector('#email')
 const msg=document.querySelector('.msg')
 const userList=document.querySelector('#users')
 const btn=document.querySelector('.btn')
+const ul=document.querySelector('.items');
 
 btn.addEventListener('click',onSubmit)
 
+let deleteButton=document.createElement('button');
+let editButton=document.createElement('button');
+            
+deleteButton.type='button';
+deleteButton.innerHTML='DELETE';
+deleteButton.className='delete';
+
+editButton.type='button';
+editButton.innerHTML='EDIT';
+editButton.className='edit';
 
 //myForm.addEventListener('click',onSubmit);
 
-let count=1;
 function onSubmit(e){
      e.preventDefault();
      if(nameInput.value==='' || emailInput.value===''){
@@ -18,21 +28,51 @@ function onSubmit(e){
          msg.style.background='orangered';
          setTimeout(()=> msg.innerHTML="",3000)
      }else{
-         const li=document.createElement('li');
-         li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`)); 
-         userList.appendChild(li);
+         // const li=document.createElement('li');
+         // li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`)); 
+         // userList.appendChild(li);
  
          let my_obj={
             'name':nameInput.value,
             'email':emailInput.value
          }
          let obj=JSON.stringify(my_obj);
-         localStorage.setItem('userDetails',obj);
-         count++;
+         
+         localStorage.setItem(my_obj.email,obj);
+         let keyArray=Object.keys(localStorage);
+
+         let li;
+         console.log(keyArray)
+         for(let datakey of keyArray){
+            let data=JSON.parse(localStorage[datakey]);
+            li=document.createElement('li');
+            li.appendChild(document.createTextNode(`${data.name}-${data.email}`));
+
+            //createing buttons
+            
+            li.appendChild(deleteButton)
+            li.appendChild(editButton)
+         
+            console.log(data)
+         }
+         ul.appendChild(li)
  
          
      }
      nameInput.value='';
      emailInput.value=''; 
  }
+ window.addEventListener('DOMContentLoaded', (event) => {
+    let keyArray=Object.keys(localStorage);
+    console.log(keyArray)
+    for(let datakey of keyArray){
+       let data=JSON.parse(localStorage[datakey]);
+       let li=document.createElement('li');
+       li.appendChild(document.createTextNode(`${data.name}-${data.email}`));
+       li.appendChild(deleteButton)
+       li.appendChild(editButton)
+       ul.appendChild(li);
+       console.log(data)
+    }
+})
  
